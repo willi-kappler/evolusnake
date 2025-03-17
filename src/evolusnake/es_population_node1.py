e39e5dd754dfe677fbb6079325650ad3ece81a5d
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-03-17 14:28:46 +0100

    Implement first two population types.

---
+++ src/evolusnake/es_population_node1.py
@@ -0,0 +1,64 @@
+# This file is part of Evolusnake, evolutionary algorithms in Python
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/evolusnake
+
+"""
+This module defines the base data class for an individual.
+"""
+
+# Python std lib:
+import logging
+from typing import Any,  override
+
+# External imports:
+from parasnake.ps_node import PSNode
+
+# Local imports:
+from evolusnake.es_config import ESConfiguration
+from evolusnake.es_individual import ESIndividual
+from evolusnake.es_population import ESPopulation
+
+logger = logging.getLogger(__name__)
+
+class ESPopulationNode1(PSNode):
+    def __init__(self, config: ESConfiguration, individual: ESIndividual):
+        super().__init__(config.parasnake_config)
+
+        self.population = ESPopulation(config, individual)
+
+        # Create working list with additional individuals:
+        for _ in range(self.population.population_size):
+            self.population.population.append(individual)
+
+    @override
+    def ps_process_data(self, data: Any) -> Any:
+        logger.debug(f"ESPopulationNode1.ps_process_data()")
+        logger.debug(f"Individual from server: {data.fitness}")
+
+        self.population.es_reset_or_accept_best(data)
+        offset = self.population.population_size
+
+        for i in range(self.population.num_of_iterations):
+            # Create a copy of each individual before mutating it:
+            for j in range(offset):
+                ind: ESIndividual = self.population.population[j]
+                self.population.population[j + offset] = ind.es_clone()
+
+                # Now mutate the original individual:
+                for _ in range(self.population.num_of_mutations):
+                    ind.es_mutate()
+                ind.es_calculate_fitness()
+
+            self.population.es_sort_population()
+
+            if self.population.population[0] <= self.population.target_fitness:
+                logger.info(f"Early exit at iteration {i}")
+                break
+
+        best_fitness: float = self.population.population[0].fitness
+        worst_fitness: float = self.population.population[offset - 1].fitness
+        logger.debug(f"{best_fitness=}, {worst_fitness=}")
+
+        return self.population.population[0]
+
---
+++ src/evolusnake/es_population_node2.py
@@ -0,0 +1,70 @@
+# This file is part of Evolusnake, evolutionary algorithms in Python
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/evolusnake
+
+"""
+This module defines the base data class for an individual.
+"""
+
+# Python std lib:
+import logging
+from typing import Any,  override
+
+# External imports:
+from parasnake.ps_node import PSNode
+
+# Local imports:
+from evolusnake.es_config import ESConfiguration
+from evolusnake.es_individual import ESIndividual
+from evolusnake.es_population import ESPopulation
+
+logger = logging.getLogger(__name__)
+
+class ESPopulationNode2(PSNode):
+    def __init__(self, config: ESConfiguration, individual: ESIndividual):
+        super().__init__(config.parasnake_config)
+
+        self.population = ESPopulation(config, individual)
+
+    @override
+    def ps_process_data(self, data: Any) -> Any:
+        logger.debug(f"ESPopulationNode2.ps_process_data()")
+        logger.debug(f"Individual from server: {data.fitness}")
+
+        self.population.es_reset_or_accept_best(data)
+        max_mutation: int = self.population.num_of_mutations
+        minimum_found: bool = False
+
+        for i in range(self.population.num_of_iterations):
+            for j in range(self.population.population_size):
+                tmp_ind: ESIndividual = self.population.population[j].es_clone()
+
+                for _ in range(max_mutation):
+                    tmp_ind.es_mutate()
+                tmp_ind.es_calculate_fitness()
+
+                if tmp_ind.fitness < self.population.population[j].fitness:
+                    self.population.population[j] = tmp_ind
+
+                    if tmp_ind.fitness <= self.population.target_fitness:
+                        logger.info(f"Early exit at iteration {i}")
+                        minimum_found = True
+                        break
+
+            if minimum_found:
+                break
+
+            # change mutation rate:
+            max_mutation -= 1
+            if max_mutation <= 0:
+                max_mutation = self.population.num_of_mutations
+
+        self.population.es_find_best_and_worst_individual()
+
+        best_fitness: float = self.population.best_fitness
+        worst_fitness: float = self.population.worst_fitness
+        logger.debug(f"{best_fitness=}, {worst_fitness=}")
+
+        return self.population.population[self.population.best_index]
+
