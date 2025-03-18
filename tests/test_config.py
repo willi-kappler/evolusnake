9107ee99e637d39373dbef21956d36d3fbd00f64
Willi Kappler <willi.kappler.gm@gmail.com>
2025-03-18 19:46:07 +0100

    Add comments for imports.

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,10 +3,12 @@
 #
 # See: https://github.com/willi-kappler/evolusnake
 
+# Python std lib:
 import unittest
 import json
 import os
 
+# Local imports:
 from evolusnake.es_config import ESConfiguration
 
 
