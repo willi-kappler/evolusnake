c7ac8078efd7ce93fd9b3e09d307154dedf5c810
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-03-20 12:09:39 +0100

    Change some settinsg.

--- examples/tsp/run_example.sh
+++ examples/tsp/run_example.sh
@@ -6,10 +6,10 @@ export PYTHONPATH=$PYTHONPATH:"../../src/"
 
 python3 tsp_main.py --server &
 sleep 2
-python3 tsp_main.py --num_of_mutations 1 &
+python3 tsp_main.py &
 sleep 2
-python3 tsp_main.py --num_of_mutations 10 &
+python3 tsp_main.py --reset_population &
 sleep 2
-python3 tsp_main.py --reset_population --num_of_mutations 1 &
+python3 tsp_main.py --reset_population &
 sleep 2
-python3 tsp_main.py --reset_population --num_of_mutations 10 &
+python3 tsp_main.py --reset_population &
