c7514691b5c57379daac5c59f63e58bfc2ed8070
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-03-20 11:22:01 +0100

    Add command line arguments.

--- examples/tsp/run_example.sh
+++ examples/tsp/run_example.sh
@@ -4,13 +4,12 @@ reset
 
 export PYTHONPATH=$PYTHONPATH:"../../src/"
 
-python3 main.py --server &
+python3 tsp_main.py --server &
 sleep 2
-python3 main.py &
+python3 tsp_main.py --num_of_mutations 1 &
 sleep 2
-python3 main.py &
+python3 tsp_main.py --num_of_mutations 10 &
 sleep 2
-python3 main.py &
+python3 tsp_main.py --reset_population --num_of_mutations 1 &
 sleep 2
-python3 main.py &
-
+python3 tsp_main.py --reset_population --num_of_mutations 10 &
