044b2128f3d1ba08a9ba8d73e1c3a1c32ebd9763
Willi Kappler <willi.kappler.gm@gmail.com>
2025-03-22 13:08:36 +0100

    Add population selector.

---
+++ src/evolusnake/es_select_population.py
@@ -0,0 +1,38 @@
+# This file is part of Evolusnake, evolutionary algorithms in Python.
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/evolusnake
+
+import logging
+
+# Local imports:
+from evolusnake.es_config import ESConfiguration
+from evolusnake.es_individual import ESIndividual
+from evolusnake.es_population_node1 import ESPopulationNode1
+from evolusnake.es_population_node2 import ESPopulationNode2
+from evolusnake.es_population_node3 import ESPopulationNode3
+from evolusnake.es_population_node4 import ESPopulationNode4
+
+# External imports:
+from parasnake.ps_node import PSNode
+
+
+logger = logging.getLogger(__name__)
+
+
+def es_select_population(configuration: ESConfiguration, individual: ESIndividual) -> PSNode:
+    pop_kind: int = configuration.population_kind
+
+    match pop_kind:
+        case 1:
+            return ESPopulationNode1(configuration, individual)
+        case 2:
+            return ESPopulationNode2(configuration, individual)
+        case 3:
+            return ESPopulationNode3(configuration, individual)
+        case 4:
+            return ESPopulationNode4(configuration, individual)
+        case _:
+            raise ValueError(f"Unknown population kind: {pop_kind}")
+
+
