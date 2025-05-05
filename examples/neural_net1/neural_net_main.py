15285fb9749d544279109569c96fe85dc0f27c33
Willi Kappler <willi.kappler.gm@gmail.com>
2025-05-05 21:52:29 +0200

    Refacot rename

--- examples/neural_net1/neural_net_main.py
+++ examples/neural_net1/neural_net_main.py
@@ -166,10 +166,12 @@ class NeuralNetIndividual(ESIndividual):
         self.evaluate_with_error(([1.0, 0.0], [1.0]))
         self.evaluate_with_error(([1.0, 1.0], [0.0]))
 
-        for values in self.data_provider.training_batch():
-            self.evaluate_with_error(values)
+        # for values in self.data_provider.training_batch():
+        #     self.evaluate_with_error(values)
+        #
+        # self.fitness = self.fitness / (4.0 + self.data_provider.batch_size)
 
-        self.fitness = self.fitness / (4.0 + self.data_provider.batch_size)
+        self.fitness = self.fitness / 4.0
 
     @override
     def es_clone(self) -> Self:
