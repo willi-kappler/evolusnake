9dc6a2a662210d1fbac73c70ffc14567805fcf00
Willi Kappler <willi.kappler.gm@gmail.com>
2025-06-01 10:10:55 +0200

    Remove unused (comment) code.

--- examples/neural_net2/neural_net_base.py
+++ examples/neural_net2/neural_net_base.py
@@ -195,12 +195,6 @@ class NeuralNetBase(ESIndividual):
                 self.add_hidden_connection()
             case 2:
                 self.add_neuron()
-                # Hyperparameter
-                # prob: int = fastrand.pcg32bounded(1000)
-                # if prob == 0:
-                #     self.add_neuron()
-                # else:
-                #     return True
             case 3:
                 self.randomize_all_neurons()
             case 4:
@@ -215,12 +209,6 @@ class NeuralNetBase(ESIndividual):
                 self.prune_connections()
             case 9:
                 self.change_activation_function()
-                # Hyperparameter
-                # prob: int = fastrand.pcg32bounded(100)
-                # if prob == 0:
-                #     self.change_activation_function()
-                # else:
-                #     return True
 
         return False
 
