cb4c6d01c4f0ccfa7225bbed3ec62dab25b28145
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-06-06 13:04:15 +0200

    Change run scripts.

--- examples/neural_net2/run_example2.sh
+++ examples/neural_net2/run_example2.sh
@@ -6,38 +6,36 @@ run_kind() {
     echo "Run kind $1."
 
     num_of_mutations="1"
-    iterations1="10000"
-    iterations2="20000"
-    #iterations3="5000"
+    iterations1="3000"
 
     case $1 in
     6)
         num_of_mutations="20"
     esac
 
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=1 &
-    sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=2 &
-    sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=3 &
-    sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=4 &
-    sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=5 &
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
     sleep 2
     python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
     sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations2 --user_options net=1 &
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
     sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations2 --user_options net=2 &
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
     sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations2 --user_options net=3 &
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
     sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations2 --user_options net=4 &
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
     sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations2 --user_options net=5 &
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
     sleep 2
-    python3 $main_file -k $1 -m $num_of_mutations -i $iterations2 --user_options net=6 &
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
+    sleep 2
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
+    sleep 2
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
+    sleep 2
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
+    sleep 2
+    python3 $main_file -k $1 -m $num_of_mutations -i $iterations1 --user_options net=6 &
     sleep 2
 }
 
